be2a1735e55e480502d3c357c3b88bb535eb8278
szymonkluka <110814419+szymonkluka@users.noreply.github.com>
2022-12-15 16:01:11 +0100

    Delete Downloads directory

--- Downloads/kofi-project3-master/kofi-project3-master/src/js/utils.js
+++
@@ -1,9 +0,0 @@
-const utils = {};
-
-utils.createDOMFromHTML = function (htmlString) {
-  let div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  return div.firstChild;
-};
-
-export default utils;
--- Downloads/stopwatch-master/stopwatch-master/src/App.js
+++
@@ -1,9 +0,0 @@
-import StopWatch from './componenets/StopWatch/StopWatch';
-
-const App = () => {
-  return (
-    <StopWatch />
-  );
-};
-
-export default App;
